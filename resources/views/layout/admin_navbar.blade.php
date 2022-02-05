8017a21e4254f19eec70b245c0d5a1cc7dec9882
Nur Hidayatullah <>
2022-02-06 00:49:55 +0700

    auth middleware

--- resources/views/layout/admin_navbar.blade.php
+++ resources/views/layout/admin_navbar.blade.php
@@ -152,24 +152,11 @@
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
                 <img class="img-profile rounded-circle"
-                    src="assets/img/undraw_profile.svg">
+                    src="{{ asset('assets/img/undraw_profile.svg') }}">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                 aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Profile
-                </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Settings
-                </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Activity Log
-                </a>
-                <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="{{ route("app_logout") }}">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
--- routes/web.php
+++ routes/web.php
@@ -25,26 +25,30 @@ Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])
         ->name('app_logout')
         ->middleware('auth');
 
-Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])
-        ->name('dashboard')
-        ->middleware('auth');
+Route::middleware(['auth'])->prefix('admin')->group(function() {
+    Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])
+            ->name('dashboard')
+            ->middleware('auth');
 
-Route::controller(App\Http\Controllers\UserController::class)->prefix('user')->group(function () {
-    Route::get('/', 'index')->name('user_index');
-    Route::match(['get', 'post'], '/create', 'create')->name('user_create');
-    Route::match(['get', 'post'], '/{id}/edit', 'edit')->name('user_edit');
-    Route::delete('/{id}/remove', 'remove')->name('user_remove');
+    Route::controller(App\Http\Controllers\UserController::class)->prefix('user')->group(function () {
+        Route::get('/', 'index')->name('user_index');
+        Route::match(['get', 'post'], '/create', 'create')->name('user_create');
+        Route::match(['get', 'post'], '/{id}/edit', 'edit')->name('user_edit');
+        Route::delete('/{id}/remove', 'remove')->name('user_remove');
+    });
+
+    Route::controller(App\Http\Controllers\PenulisController::class)->prefix('penulis')->group(function () {
+        Route::get('/', 'index')->name('penulis_index');
+        Route::match(['get', 'post'], '/create', 'create')->name('penulis_create');
+        Route::match(['get', 'post'], '/{id}/edit', 'edit')->name('penulis_edit');
+        Route::delete('/{id}/remove', 'remove')->name('penulis_remove');
+    });
+    Route::controller(App\Http\Controllers\BukuController::class)->prefix('buku')->group(function () {
+        Route::get('/', 'index')->name('buku_index');
+        Route::match(['get', 'post'], '/create', 'create')->name('buku_create');
+        Route::match(['get', 'post'], '/{id}/edit', 'edit')->name('buku_edit');
+        Route::delete('/{id}/remove', 'remove')->name('buku_remove');
+    });
 });
 
-Route::controller(App\Http\Controllers\PenulisController::class)->prefix('penulis')->group(function () {
-    Route::get('/', 'index')->name('penulis_index');
-    Route::match(['get', 'post'], '/create', 'create')->name('penulis_create');
-    Route::match(['get', 'post'], '/{id}/edit', 'edit')->name('penulis_edit');
-    Route::delete('/{id}/remove', 'remove')->name('penulis_remove');
-});
-Route::controller(App\Http\Controllers\BukuController::class)->prefix('buku')->group(function () {
-    Route::get('/', 'index')->name('buku_index');
-    Route::match(['get', 'post'], '/create', 'create')->name('buku_create');
-    Route::match(['get', 'post'], '/{id}/edit', 'edit')->name('buku_edit');
-    Route::delete('/{id}/remove', 'remove')->name('buku_remove');
-});
+    
